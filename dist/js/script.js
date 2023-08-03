ea881631fa7f40829177467aa467dfd25c239a33
KevinAsar <kevinasar112211@gmail.com>
2023-08-03 17:22:18 +0700

    Default theme

--- dist/js/script.js
+++ dist/js/script.js
@@ -48,10 +48,6 @@ darkToggle.addEventListener("click", function () {
 
 // pindahkan posisi toggle sesuai mode
 
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme:)').matches)) {
-  darkToggle.checked = false;
-} else {
-  darkToggle.checked = false;
-}
+
 
 
